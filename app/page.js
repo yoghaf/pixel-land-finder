2260101514e414be696c0c82084ad070f6ab5c0c
yoghaf <utomoyogafirmansyah@gmail.com>
2024-02-27 08:44:09 +0700

    update add more view

--- app/page.js
+++ app/page.js
@@ -10,14 +10,75 @@ export default function Home() {
   const [numberLandThree, setNumberLandThree] = useState(1);
   const [landNumberFour, setLandNumberFour] = useState(1);
   const [numberLandFour, setNumberLandFour] = useState(1);
+  const [landNumberFive, setLandNumberFive] = useState(1);
+  const [numberLandFive, setNumberLandFive] = useState(1);
+  const [landNumberSix, setLandNumberSix] = useState(1);
+  const [numberLandSix, setNumberLandSix] = useState(1);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-20">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <h1 className="font-semibold text-2xl">Pixel find land by Utomo Yoga</h1>
-      <div className="grid grid-cols-2  h-screen w-full">
+
+      <div className="p-5 w-1/3 flex justify-between">
+        <button
+          type="button"
+          onClick={() => {
+            const landtonumberOne = parseInt(numberLandOne);
+            const landtonumberTwo = parseInt(numberLandTwo);
+            const landtonumberThree = parseInt(numberLandThree);
+            const landtonumberFour = parseInt(numberLandFour);
+            const landtonumberFive = parseInt(numberLandFive);
+            const landtonumberSix = parseInt(numberLandSix);
+            setLandNumberOne(landtonumberOne - 1);
+            setNumberLandOne(landtonumberOne - 1);
+            setLandNumberTwo(landtonumberTwo - 1);
+            setNumberLandTwo(landtonumberTwo - 1);
+            setLandNumberThree(landtonumberThree - 1);
+            setNumberLandThree(landtonumberThree - 1);
+            setLandNumberFour(landtonumberFour - 1);
+            setNumberLandFour(landtonumberFour - 1);
+            setLandNumberFive(landtonumberFive - 1);
+            setNumberLandFive(landtonumberFive - 1);
+            setLandNumberSix(landtonumberSix - 1);
+            setNumberLandSix(landtonumberSix - 1);
+          }}
+          className="bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Prev All
+        </button>
+
+        <button
+          type="button"
+          onClick={() => {
+            const landtonumberOne = parseInt(numberLandOne);
+            const landtonumberTwo = parseInt(numberLandTwo);
+            const landtonumberThree = parseInt(numberLandThree);
+            const landtonumberFour = parseInt(numberLandFour);
+            const landtonumberFive = parseInt(numberLandFive);
+            const landtonumberSix = parseInt(numberLandSix);
+            setLandNumberOne(landtonumberOne + 1);
+            setNumberLandOne(landtonumberOne + 1);
+            setLandNumberTwo(landtonumberTwo + 1);
+            setNumberLandTwo(landtonumberTwo + 1);
+            setLandNumberThree(landtonumberThree + 1);
+            setNumberLandThree(landtonumberThree + 1);
+            setLandNumberFour(landtonumberFour + 1);
+            setNumberLandFour(landtonumberFour + 1);
+            setLandNumberFive(landtonumberFive + 1);
+            setNumberLandFive(landtonumberFive + 1);
+            setLandNumberSix(landtonumberSix + 1);
+            setNumberLandSix(landtonumberSix + 1);
+          }}
+          className="bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Next All
+        </button>
+      </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-3  h-screen w-full">
         {/* number one */}
         <div className="  items-center p-2  w-full">
-          <div className=" flex flex-col h-[500px]">
+          <div className=" flex flex-col h-full">
             <div>
               <form
                 onSubmit={(e) => {
@@ -265,6 +326,136 @@ export default function Home() {
             <div style={{ overflow: "auto", width: "100%", height: "100%" }}>
               <iframe
                 src={`https://play.pixels.xyz/pixels/share/${landNumberFour}`}
+                style={{
+                  transform: "scale(1.6)",
+                  transformOrigin: "0 0",
+                  width: "calc(100% / 1.6)",
+                  height: "calc(100% / 1.6)",
+                }}
+              ></iframe>
+            </div>
+          </div>
+        </div>
+        {/* number five */}
+        <div className="  items-center p-2  w-full">
+          <div className=" flex flex-col h-[500px]">
+            <div>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  setLandNumberFive(numberLandFive);
+                }}
+                className=" flex gap-2 p-3"
+              >
+                <label className="block">
+                  <input
+                    onChange={(e) => {
+                      setNumberLandFive(e.target.value);
+                    }}
+                    className="h-10 w-full  border border-gray-300 rounded-md px-3"
+                    type="number"
+                    name="name"
+                    placeholder="Enter land"
+                  />
+                </label>
+                <button type="submit" className="bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  Find
+                </button>
+              </form>
+              <div className="p-3 flex justify-between">
+                <button
+                  type="button"
+                  onClick={() => {
+                    const landtonumber = parseInt(numberLandFive);
+                    setLandNumberFive(landtonumber - 1);
+                    setNumberLandFive(landtonumber - 1);
+                  }}
+                  className="bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Prev
+                </button>
+                <h3>{landNumberFive}</h3>
+                <button
+                  type="button"
+                  onClick={() => {
+                    const landtonumber = parseInt(numberLandFive);
+                    setLandNumberFive(landtonumber + 1);
+                    setNumberLandFive(landtonumber + 1);
+                  }}
+                  className="bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Next
+                </button>
+              </div>
+            </div>
+            <div style={{ overflow: "auto", width: "100%", height: "100%" }}>
+              <iframe
+                src={`https://play.pixels.xyz/pixels/share/${landNumberFive}`}
+                style={{
+                  transform: "scale(1.6)",
+                  transformOrigin: "0 0",
+                  width: "calc(100% / 1.6)",
+                  height: "calc(100% / 1.6)",
+                }}
+              ></iframe>
+            </div>
+          </div>
+        </div>
+        {/* number six */}
+        <div className="  items-center p-2  w-full">
+          <div className=" flex flex-col h-[500px]">
+            <div>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  setLandNumberSix(numberLandSix);
+                }}
+                className=" flex gap-2 p-3"
+              >
+                <label className="block">
+                  <input
+                    onChange={(e) => {
+                      setNumberLandSix(e.target.value);
+                    }}
+                    className="h-10 w-full  border border-gray-300 rounded-md px-3"
+                    type="number"
+                    name="name"
+                    placeholder="Enter land"
+                  />
+                </label>
+                <button type="submit" className="bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  Find
+                </button>
+              </form>
+              <div className="p-3 flex justify-between">
+                <button
+                  type="button"
+                  onClick={() => {
+                    const landtonumber = parseInt(numberLandSix);
+                    setLandNumberSix(landtonumber - 1);
+                    setNumberLandSix(landtonumber - 1);
+                  }}
+                  className="bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Prev
+                </button>
+                <h3>{landNumberSix}</h3>
+                <button
+                  type="button"
+                  onClick={() => {
+                    const landtonumber = parseInt(numberLandSix);
+                    setLandNumberSix(landtonumber + 1);
+                    setNumberLandSix(landtonumber + 1);
+                  }}
+                  className="bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Next
+                </button>
+              </div>
+            </div>
+            <div style={{ overflow: "auto", width: "100%", height: "100%" }}>
+              <iframe
+                src={`https://play.pixels.xyz/pixels/share/${landNumberSix}`}
                 style={{
                   transform: "scale(1.6)",
                   transformOrigin: "0 0",
