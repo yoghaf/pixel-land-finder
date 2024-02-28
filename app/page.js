0105f0722b61178eb0eb8e79730c4a260c627128
yoghaf <utomoyogafirmansyah@gmail.com>
2024-02-28 09:15:40 +0700

    add randomize land

--- app/page.js
+++ app/page.js
@@ -1,24 +1,66 @@
 "use client";
 import { useState, useEffect } from "react";
 
-export default function Home() {
-  const [landNumberOne, setLandNumberOne] = useState(1);
-  const [numberLandOne, setNumberLandOne] = useState(1);
-  const [landNumberTwo, setLandNumberTwo] = useState(1);
-  const [numberLandTwo, setNumberLandTwo] = useState(1);
-  const [landNumberThree, setLandNumberThree] = useState(1);
-  const [numberLandThree, setNumberLandThree] = useState(1);
-  const [landNumberFour, setLandNumberFour] = useState(1);
-  const [numberLandFour, setNumberLandFour] = useState(1);
-  const [landNumberFive, setLandNumberFive] = useState(1);
-  const [numberLandFive, setNumberLandFive] = useState(1);
-  const [landNumberSix, setLandNumberSix] = useState(1);
-  const [numberLandSix, setNumberLandSix] = useState(1);
+export default function Home({ initialLandNumbers }) {
+  const [landNumberOne, setLandNumberOne] = useState(initialLandNumbers ? initialLandNumbers[0] : null);
+  const [numberLandOne, setNumberLandOne] = useState(generateRandomNumber());
+  const [landNumberTwo, setLandNumberTwo] = useState(initialLandNumbers ? initialLandNumbers[1] : null);
+  const [numberLandTwo, setNumberLandTwo] = useState(generateRandomNumber());
+  const [landNumberThree, setLandNumberThree] = useState(initialLandNumbers ? initialLandNumbers[2] : null);
+  const [numberLandThree, setNumberLandThree] = useState(generateRandomNumber());
+  const [landNumberFour, setLandNumberFour] = useState(initialLandNumbers ? initialLandNumbers[3] : null);
+  const [numberLandFour, setNumberLandFour] = useState(generateRandomNumber());
+  const [landNumberFive, setLandNumberFive] = useState(initialLandNumbers ? initialLandNumbers[4] : null);
+  const [numberLandFive, setNumberLandFive] = useState(generateRandomNumber());
+  const [landNumberSix, setLandNumberSix] = useState(initialLandNumbers ? initialLandNumbers[5] : null);
+  const [numberLandSix, setNumberLandSix] = useState(generateRandomNumber());
+
+  useEffect(() => {
+    if (initialLandNumbers) {
+      setLandNumberOne(initialLandNumbers[0]);
+      setLandNumberTwo(initialLandNumbers[1]);
+      setLandNumberThree(initialLandNumbers[2]);
+      setLandNumberFour(initialLandNumbers[3]);
+      setLandNumberFive(initialLandNumbers[4]);
+      setLandNumberSix(initialLandNumbers[5]);
+    }
+  }, []);
+
+  function generateRandomNumber() {
+    return Math.floor(Math.random() * 5000) + 1;
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <h1 className="font-semibold text-2xl">Pixel find land by Utomo Yoga</h1>
-
+      <div className="p-5 ">
+        <button
+          onClick={() => {
+            const landtonumberOne = parseInt(numberLandOne);
+            const landtonumberTwo = parseInt(numberLandTwo);
+            const landtonumberThree = parseInt(numberLandThree);
+            const landtonumberFour = parseInt(numberLandFour);
+            const landtonumberFive = parseInt(numberLandFive);
+            const landtonumberSix = parseInt(numberLandSix);
+            setLandNumberOne(landtonumberOne + 1);
+            setNumberLandOne(landtonumberOne + 1);
+            setLandNumberTwo(landtonumberTwo + 1);
+            setNumberLandTwo(landtonumberTwo + 1);
+            setLandNumberThree(landtonumberThree + 1);
+            setNumberLandThree(landtonumberThree + 1);
+            setLandNumberFour(landtonumberFour + 1);
+            setNumberLandFour(landtonumberFour + 1);
+            setLandNumberFive(landtonumberFive + 1);
+            setNumberLandFive(landtonumberFive + 1);
+            setLandNumberSix(landtonumberSix + 1);
+            setNumberLandSix(landtonumberSix + 1);
+          }}
+          type="button"
+          className="bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Random All
+        </button>
+      </div>
       <div className="p-5 w-1/3 flex justify-between">
         <button
           type="button"
@@ -470,3 +512,8 @@ export default function Home() {
     </main>
   );
 }
+
+Home.getInitialProps = async () => {
+  const initialLandNumbers = Array(6).fill(null).map(generateRandomNumber);
+  return { initialLandNumbers };
+};
